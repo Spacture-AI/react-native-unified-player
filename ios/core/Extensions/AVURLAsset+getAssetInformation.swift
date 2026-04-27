ea24c71b7fb36afd0225996fa949f8405ec8a7dd
Yaşar Özyurt <blueromans@hotmail.com>
2026-04-27 16:25:13 +0300

    refactor: streamline asset information retrieval in AVURLAsset extension

--- ios/core/Extensions/AVURLAsset+getAssetInformation.swift
+++ ios/core/Extensions/AVURLAsset+getAssetInformation.swift
@@ -2,67 +2,66 @@ import AVFoundation
 
 extension AVURLAsset {
   func getAssetInformation() async throws -> VideoInformation {
-    // Initialize with default values
-    var videoInformation = VideoInformation(
-      bitrate: Double.nan,
-      width: Double.nan,
-      height: Double.nan,
-      duration: -1,
-      fileSize: -1,
-      isHDR: false,
-      isLive: false,
-      orientation: .unknown
-    )
+    var bitrate: Double = .nan
+    var width: Double = .nan
+    var height: Double = .nan
+    var durationSeconds: Int64 = -1
+    var isHDR = false
+    var isLive = false
+    var orientation: VideoOrientation = .unknown
 
-    videoInformation.fileSize = try await VideoFileHelper.getFileSize(for: url)
+    let fileSize = try await VideoFileHelper.getFileSize(for: url)
 
-    // Check if asset is live stream
     if duration.flags.contains(.indefinite) {
-      videoInformation.duration = -1
-      videoInformation.isLive = true
+      durationSeconds = -1
+      isLive = true
     } else {
-      videoInformation.duration = Int64(CMTimeGetSeconds(duration))
-      videoInformation.isLive = false
+      durationSeconds = Int64(CMTimeGetSeconds(duration))
+      isLive = false
     }
 
     if let videoTrack = tracks(withMediaType: .video).first {
       let size = videoTrack.naturalSize.applying(videoTrack.preferredTransform)
-      videoInformation.width = size.width
-      videoInformation.height = size.height
-
-      videoInformation.bitrate = Double(videoTrack.estimatedDataRate)
-
-      videoInformation.orientation = videoTrack.orientation
+      width = size.width
+      height = size.height
+      bitrate = Double(videoTrack.estimatedDataRate)
+      orientation = videoTrack.orientation
 
       if #available(iOS 14.0, tvOS 14.0, visionOS 1.0, *) {
-        videoInformation.isHDR = videoTrack.hasMediaCharacteristic(.containsHDRVideo)
+        isHDR = videoTrack.hasMediaCharacteristic(.containsHDRVideo)
       }
     } else if url.pathExtension == "m3u8" {
-      // For HLS streams, we cannot get video track information directly
-      // So we download manifest and try to extract video information from it
-
+      // For HLS streams, we cannot get video track information directly,
+      // so we download the manifest and try to extract video info from it.
       let manifestContent = try await HLSManifestParser.downloadManifest(from: url)
       let manifestInfo = try HLSManifestParser.parseM3U8Manifest(manifestContent)
 
       if let videoStream = manifestInfo.streams.first {
-        videoInformation.width = Double(videoStream.width ?? Int(Double.nan))
-        videoInformation.height = Double(videoStream.height ?? Int(Double.nan))
-        videoInformation.bitrate = Double(videoStream.bandwidth ?? Int(Double.nan))
+        width = Double(videoStream.width ?? Int(Double.nan))
+        height = Double(videoStream.height ?? Int(Double.nan))
+        bitrate = Double(videoStream.bandwidth ?? Int(Double.nan))
       }
 
-      if videoInformation.width > 0 && videoInformation.height > 0 {
-        if videoInformation.width == videoInformation.height {
-          videoInformation.orientation = .square
-        } else if videoInformation.width > videoInformation.height {
-          videoInformation.orientation = .landscapeRight
-        } else if videoInformation.width < videoInformation.height {
-          videoInformation.orientation = .portrait
+      if width > 0 && height > 0 {
+        if width == height {
+          orientation = .square
+        } else if width > height {
+          orientation = .landscapeRight
         } else {
-          videoInformation.orientation = .unknown
+          orientation = .portrait
         }
       }
     }
 
-    return videoInformation
+    return VideoInformation(
+      bitrate: bitrate,
+      width: width,
+      height: height,
+      duration: durationSeconds,
+      fileSize: fileSize,
+      isHDR: isHDR,
+      isLive: isLive,
+      orientation: orientation
+    )
   }
 }
